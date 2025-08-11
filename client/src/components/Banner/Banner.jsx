66051b74fe04e88c4551beef4da08354e32480ef
Arbazhasan <arbaazhasan.ah@gmail.com>
2025-08-11 11:58:17 +0530

    Update Header Css Remove Tailwind and set Pixel to Pixel with Figma Design

---
+++ client/src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='bd w-[1384px] h-135 rounded-3xl bg-gradient-to-r from-blue-600 to-blue-900'>
+
+        </div>
+    );
+}
+
+export default Banner;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./header.scss"
+
 import { IoLocationOutline } from "react-icons/io5";
 import { FaPlus } from "react-icons/fa6";
 import { LuMousePointerClick } from "react-icons/lu";
@@ -13,94 +15,165 @@ import { FaRegBell } from "react-icons/fa";
 
 const Header = () => {
     return (
-        <div className="h-20 p-19 px-15 flex gap-8 items-center justify-between">
+        // <div className="h-[104.98761749267578px] p-19 px-15 flex gap-8 items-center justify-between">
 
-            <div className=" h-23 flex items-center justify-center">
-                <img src="/images/logo.png" alt="Logo" />
+        //     <div className="flex items-center justify-center">
+        //         <img className='w-[125.87073516845703px] h-[49.52291488647461px] ' src="/images/logo.jpg" alt="Logo" />
+        //     </div>
+
+        //     <div className="border-2 border-solid border-gray-500  px-2.5 rounded-xl flex gap-1.5 items-center ">
+        //         <IoLocationOutline className="text-[24px]" />
+        //         <input
+        //             type="text"
+        //             placeholder="Taj Garden Retreat, Bengaluru"
+        //             className="text-[16px] outline-none text-black w-full"
+        //         />
+        //     </div>
+
+        //     <div className='flex gap-8'>
+        //         <button className=" min-w-17 rounded-3xl p-1 bg-gradient-to-r from-blue-600 to-blue-950">
+        //             <div className='h-8 px-3 bg-white rounded-3xl flex items-center justify-center  gap-1.5'>
+        //                 <FaPlus />
+        //                 Sell
+        //             </div>
+        //         </button>
+
+        //         <button className=" min-w-17 rounded-3xl p-1 bg-gradient-to-r from-blue-600 to-blue-950">
+        //             <div className='h-8 px-3 bg-white rounded-3xl flex items-center justify-center  gap-1.5'>
+        //                 <LuMousePointerClick />
+        //                 One Click sell
+        //             </div>
+        //         </button>
+        //     </div>
+
+        //     <div className=' min-w-100 flex justify-between'>
+
+        //         <a href=""
+        //             className='flex flex-col items-center gap-0.5 '
+        //         >
+
+        //             <AiOutlineShopping className='text-2xl text-gray-600' />
+        //             <p className='text-[10px] text-gray-500'>Your Items</p>
+        //         </a>
+
+        //         <a href=""
+        //             className='flex flex-col items-center gap-0.5 '
+        //         >
+
+        //             <CiHeart className='text-2xl text-gray-600' />
+        //             <p className='text-xs text-gray-500'>Favourites</p>
+        //         </a>
+
+        //         <a href=""
+        //             className='flex flex-col items-center gap-0.5 '
+        //         >
+
+        //             <IoChatboxEllipsesOutline className='text-2xl text-gray-600' />
+        //             <p className='text-xs text-gray-500'>Chat</p>
+        //         </a>
+
+        //         <a href=""
+        //             className='flex flex-col items-center gap-0.5 '
+        //         >
+
+        //             <IoIosPaper className='text-2xl text-gray-600' />
+        //             <p className='text-xs text-gray-500'>Blogs</p>
+        //         </a>
+
+        //         <a href=""
+        //             className='flex flex-col items-center gap-0.5 '
+        //         >
+
+        //             <MdBusinessCenter className='text-2xl text-gray-600' />
+        //             <p className='text-xs text-gray-500'>Career</p>
+        //         </a>
+
+        //     </div>
+
+        //     <div className='flex gap-5 items-center'>
+        //         <div>
+        //             <FaRegBell className='text-3xl' />
+        //         </div>
+
+        //         <div className='border-gray-300 border-solid border-2  w-12 h-12  flex justify-center items-center rounded-full overflow-hidden cursor-pointer'>
+        //             <img src="/images/profile-icon.png" alt="" />
+        //         </div>
+        //     </div>
+
+
+
+        // </div>
+
+
+        <div className="header-container">
+
+            <div className="logo-container">
+                <img className='logo-image' src="/images/logo.jpg" alt="Logo" />
+                <p>Buy. Inspect. Deal</p>
             </div>
 
-            <div className="border-2 border-solid border-gray-500 h-10 min-w-[300px] px-2.5 rounded-xl flex gap-1.5 items-center ">
-                <IoLocationOutline className="text-lg" />
+            <div className="location-input-container">
+                <IoLocationOutline className="location-icon" />
                 <input
                     type="text"
                     placeholder="Taj Garden Retreat, Bengaluru"
-                    className="text-lg outline-none text-black w-full"
+                    className="location-input"
                 />
             </div>
 
-            <div className='flex gap-8'>
-                <button className=" min-w-17 rounded-3xl p-1 bg-gradient-to-r from-blue-600 to-blue-950">
-                    <div className='h-8 px-3 bg-white rounded-3xl flex items-center justify-center  gap-1.5'>
-                        <FaPlus />
-                        Sell
-                    </div>
-                </button>
+            <div className='sell-buttons'>
+                <div className="sell-btn">
+                    <FaPlus />
+                    Sell
+                </div>
 
-                <button className=" min-w-17 rounded-3xl p-1 bg-gradient-to-r from-blue-600 to-blue-950">
-                    <div className='h-8 px-3 bg-white rounded-3xl flex items-center justify-center  gap-1.5'>
-                        <LuMousePointerClick />
-                        One Click sell
-                    </div>
-                </button>
+                <div className="sell-btn">
+                    <LuMousePointerClick />
+                    One Click sell
+                </div>
             </div>
 
-            <div className=' min-w-100 flex justify-between'>
+            <div className='menu-links'>
 
-                <a href=""
-                    className='flex flex-col items-center gap-0.5 '
-                >
-
-                    <AiOutlineShopping className='text-2xl text-gray-600' />
-                    <p className='text-[10px] text-gray-500'>Your Items</p>
+                <a href="" className='menu-link'>
+                    <AiOutlineShopping className='menu-icon' />
+                    <p>Your Items</p>
                 </a>
 
-                <a href=""
-                    className='flex flex-col items-center gap-0.5 '
-                >
-
-                    <CiHeart className='text-2xl text-gray-600' />
-                    <p className='text-xs text-gray-500'>Favourites</p>
+                <a href="" className='menu-link'>
+                    <CiHeart className='menu-icon' />
+                    <p>Favourites</p>
                 </a>
 
-                <a href=""
-                    className='flex flex-col items-center gap-0.5 '
-                >
-
-                    <IoChatboxEllipsesOutline className='text-2xl text-gray-600' />
-                    <p className='text-xs text-gray-500'>Chat</p>
+                <a href="" className='menu-link'>
+                    <IoChatboxEllipsesOutline className='menu-icon' />
+                    <p>Chat</p>
                 </a>
 
-                <a href=""
-                    className='flex flex-col items-center gap-0.5 '
-                >
-
-                    <IoIosPaper className='text-2xl text-gray-600' />
-                    <p className='text-xs text-gray-500'>Blogs</p>
+                <a href="" className='menu-link'>
+                    <IoIosPaper className='menu-icon' />
+                    <p>Blogs</p>
                 </a>
 
-                <a href=""
-                    className='flex flex-col items-center gap-0.5 '
-                >
-
-                    <MdBusinessCenter className='text-2xl text-gray-600' />
-                    <p className='text-xs text-gray-500'>Career</p>
+                <a href="" className='menu-link'>
+                    <MdBusinessCenter className='menu-icon' />
+                    <p>Career</p>
                 </a>
 
             </div>
 
-            <div className='flex gap-5 items-center'>
+            <div className='notification-profile'>
                 <div>
-                    <FaRegBell className='text-3xl' />
+                    <FaRegBell className='notification-icon' />
                 </div>
 
-                <div className='border-gray-300 border-solid border-2  w-12 h-12  flex justify-center items-center rounded-full overflow-hidden cursor-pointer'>
-                    <img src="/images/profile-icon.jpg" alt="" />
+                <div className='profile-container'>
+                    <img src="/images/profile-icon.png" alt="" />
                 </div>
             </div>
-
-
 
         </div>
+
 
     );
 }
